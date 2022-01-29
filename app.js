0a0ef4568599c71fd0a2a3e4b5c61393bf329a32
djpetrus <96924417+djpetrus@users.noreply.github.com>
2022-01-29 17:43:36 -0300

    verao firebase

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const miniLinkRoutes = require("./routes/miniLink-routes");
+const decode = require("base-64");
+const encode = require("base-64");
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 const app = express();
 
